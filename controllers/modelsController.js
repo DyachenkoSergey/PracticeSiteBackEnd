f4b8415ccdef10ed7d80edf2f44d1bf0e9989163
Sergey Dyachenko <diachenko.serhii@nixsolutions.com>
2022-02-08 14:02:41 +0200

    before crypto

--- controllers/modelsController.js
+++ controllers/modelsController.js
@@ -22,6 +22,154 @@ class modelsController {
     }
   }
 
+  async getFilteredModels(req, res) {
+    console.log(req.query);
+    try {
+      const models = await User.find({ userRole: "MODEL" });
+      let filterModel = [];
+      const ages = {
+        Teenagers: [17, 23],
+        Young: [22, 31],
+        Mom: [30, 41],
+        Mature: [40, 51],
+        Granny: [50, 80],
+      };
+      if (req.query.age) {
+        filterModel = models.filter(
+          (model) =>
+            model.userProfile?.age > ages[req.query.age][0] &&
+            model.userProfile?.age < ages[req.query.age][1]
+        );
+      }
+      if (req.query.bodyType) {
+        filterModel = models.filter(
+          (model) => model.userProfile?.bodyType === req.query.bodyType
+        );
+      }
+      if (req.query.hair) {
+        filterModel = models.filter(
+          (model) => model.userProfile?.hair === req.query.hair
+        );
+      }
+      if (req.query.ethnicity) {
+        filterModel = models.filter(
+          (model) => model.userProfile?.ethnicity === req.query.ethnicity
+        );
+      }
+
+      if (
+        (req.query.age ||
+          req.query.bodyType ||
+          req.query.hair ||
+          req.query.ethnicity) &&
+        filterModel.length === 0
+      ) {
+        return res.json([]);
+      }
+
+      let searchModels = [];
+      if (filterModel.length) {
+        searchModels = filterModel.filter((model) =>
+          model.userName.includes(req.query.searchQueryParam)
+        );
+      } else
+        searchModels = models.filter((model) =>
+          model.userName.includes(req.query.searchQueryParam)
+        );
+      const modelInfo = searchModels.map((model) => {
+        const modelObj = {
+          userName: model.userName,
+          userEmail: model.userEmail,
+          userId: model.userId,
+        };
+        return modelObj;
+      });
+      res.json(modelInfo);
+    } catch (error) {
+      res.status(401).json({ message: "something went wrong" });
+    }
+  }
+
+  async numberTab(req, res) {
+    try {
+      const models = await User.find({ userRole: "MODEL" });
+      const filterObj = {};
+      filterObj.all = models.length;
+      const ages = [
+        [17, 23],
+        [22, 31],
+        [30, 41],
+        [40, 51],
+        [50, 80],
+      ];
+      const ageLengths = ages.map((age) => {
+        const ageListLength = models.filter(
+          (model) =>
+            model.userProfile?.age > age[0] && model.userProfile?.age < age[1]
+        );
+
+        return ageListLength.length;
+      });
+      filterObj.age = {
+        Teenagers: ageLengths[0],
+        Young: ageLengths[1],
+        Mom: ageLengths[2],
+        Mature: ageLengths[3],
+        Granny: ageLengths[4],
+      };
+
+      const bodyType = ["Skinny", "Athletic", "Curvy", "Bbw"];
+      const bodyTypeLengths = bodyType.map((bodyType) => {
+        const bodyTypeListLength = models.filter(
+          (model) => model.userProfile?.bodyType === bodyType
+        );
+        return bodyTypeListLength.length;
+      });
+      filterObj.bodyType = {
+        Skinny: bodyTypeLengths[0],
+        Athletic: bodyTypeLengths[1],
+        Curvy: bodyTypeLengths[2],
+        Bbw: bodyTypeLengths[3],
+      };
+
+      const hair = ["Black", "Blond(e)", "Brown", "Ginger", "Grey", "Bald"];
+      const hairLengths = hair.map((hair) => {
+        const hairListLength = models.filter(
+          (model) => model.userProfile?.hair === hair
+        );
+        return hairListLength.length;
+      });
+      filterObj.hair = {
+        Black: hairLengths[0],
+        "Blond(e)": hairLengths[1],
+        Brown: hairLengths[2],
+        Ginger: hairLengths[3],
+        Grey: hairLengths[4],
+        Bald: hairLengths[5],
+      };
+
+      const ethnicity = ["Arab", "Indian", "Asian", "Black", "Latina", "White"];
+      const ethnicityLengths = ethnicity.map((ethnicity) => {
+        const ethnicityListLength = models.filter(
+          (model) => model.userProfile?.ethnicity === ethnicity
+        );
+        return ethnicityListLength.length;
+      });
+      filterObj.ethnicity = {
+        Arab: ethnicityLengths[0],
+        Indian: ethnicityLengths[1],
+        Asian: ethnicityLengths[2],
+        Black: ethnicityLengths[3],
+        Latina: ethnicityLengths[4],
+        White: ethnicityLengths[5],
+      };
+
+      res.json(filterObj);
+    } catch (error) {
+      res.status(401).json({ message: "something went wrong" });
+    }
+  }
+
   async getAllModels(req, res) {
     try {
       const models = await User.find({ userRole: "MODEL" });
@@ -71,8 +219,7 @@ class modelsController {
       const userId = req.body.modelId;
       const profile = req.body.values;
       const model = await User.findOne({ userId });
-      model.get("userProfile").splice(0, model.get("userProfile").length);
-      model.get("userProfile").push(profile);
+      model.userProfile = profile;
       await model.save();
       res.json({ message: "profile changed successfully" });
     } catch (error) {
--- routes/modelsRouter.js
+++ routes/modelsRouter.js
@@ -2,18 +2,14 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/modelsController");
 
-router.post("/modelsList", controller.modelsList);
-
 router.get("/modelsList", controller.getAllModels);
-
-router.post("/model", controller.model);
-
-router.post("/studioModels", controller.studioModels);
-
-router.post("/editModelProfile", controller.editModelProfile);
-
-router.post("/modelProfile", controller.modelProfile);
-
+router.get("/filterModels", controller.getFilteredModels);
 router.get("/topModels", controller.topModels);
+router.get("/getNumberOfTabModels", controller.numberTab);
+router.post("/model", controller.model);
+router.post("/modelsList", controller.modelsList);
+router.post("/studioModels", controller.studioModels);
+router.post("/editModelProfile", controller.editModelProfile);
+router.post("/modelProfile", controller.modelProfile);
 
 module.exports = router;
